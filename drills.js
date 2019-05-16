f031ea562cb006bbdcc8cba868c20130188ecbda
Loxphordex <silasishallahan@gmail.com>
2019-05-16 11:42:42 -0700

    insertAfter

--- drills.js
+++ drills.js
@@ -1,19 +1,20 @@
-const LinkedList = require("./LinkedList")
+const LinkedList = require('./LinkedList');
 
 const SSL = new LinkedList();
 
 // .2) Create a singly linked list
 
 main = () => {
-    SSL.insertLast("Apollo");
-    SSL.insertLast("Boomer");
-    SSL.insertLast("Helo");
-    SSL.insertLast("Husker");
-    SSL.insertLast("Starbuck");
-    SSL.insertLast("Tauhida");
-    // SSL.remove("squirrel");
-    SSL.insertBefore("Helo","Y");
-    console.log(JSON.stringify(SSL))
-}
+  SSL.insertLast('Apollo');
+  SSL.insertLast('Boomer');
+  SSL.insertLast('Helo');
+  SSL.insertLast('Husker');
+  SSL.insertLast('Starbuck');
+  SSL.insertLast('Tauhida');
+  // SSL.remove("squirrel");
+  SSL.insertBefore('Helo','Y');
+  SSL.insertAfter('Starbuck', 'AfterThing');
+  console.log(JSON.stringify(SSL));
+};
 
 main();

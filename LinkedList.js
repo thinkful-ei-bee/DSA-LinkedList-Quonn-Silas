b6505f846283a81497db9e350a8d4eb5c0be06a9
quonn <qber83@gmail.com>
2019-05-17 00:28:55 -0700

    final attempts

--- LinkedList.js
+++ LinkedList.js
@@ -1,11 +1,11 @@
 class _Node {
-  constructor(value, next) {
+  constructor(value, next, prev) {
     this.value = value,
     this.next = next;
+    this.prev = prev;
   }
 }
-
-
+  
 // Linked list class
 class LinkedList {
   constructor() {
@@ -36,9 +36,9 @@ class LinkedList {
       while (tempNode.next !== null) {
         tempNode = tempNode.next;
       }
-      tempNode.next = new _Node(item, null);
+      tempNode.next = new _Node(item, null, tempNode);
     }
-
+  
   }
 
   // to retrieve a node, you start at the beginning and 
@@ -115,7 +115,7 @@ class LinkedList {
 
     //if we have found the item we are lookign for, create new node at current node position
     if(currNode.value === item){
-      let newNode = new _Node(newItem, currNode);
+      let newNode = new _Node(newItem, currNode, previousNode);
 
       //point newNode at "current" node position
       newNode.next = currNode;
@@ -124,7 +124,6 @@ class LinkedList {
       previousNode.next = newNode;
     }
     if (currNode === null) {
-      // this.head = new _Node()
       console.log('Called insertBefore(), but item not found');
       return;
     }
@@ -140,7 +139,7 @@ class LinkedList {
     }
 
     if (previousNode.value === item) {
-      let newNode = new _Node(newItem, currNode);
+      let newNode = new _Node(newItem, currNode, previousNode);
       previousNode.next = newNode;
     }
 
@@ -162,7 +161,7 @@ class LinkedList {
     }
 
     if (index === position) {
-      let newNode = new _Node(item, currNode);
+      let newNode = new _Node(item, currNode, previousNode);
       previousNode.next = newNode;
     }
 
@@ -177,4 +176,184 @@ class LinkedList {
 
 }
 
-module.exports = LinkedList;
+// module.exports = LinkedList;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+class single_Node {
+    constructor(value, next, prev) {
+      this.value = value,
+      this.next = next;
+    }
+  }
+
+// Linked list class
+class SinglyLinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    // insert a node a the beginning of the list
+    insertFirst(item) {
+      // create a new node then...
+      //assign the head to the address of the new node
+      this.head = new single_Node(item, this.head);
+    }
+  
+    // 
+    insertLast(item) {
+  
+      //check if list is empty, if it is insert new item at 
+      //  the start of the list
+      if (this.head === null) {
+        this.insertFirst(item);
+      }
+  
+      //if its not empty cycle traverse through the list until 
+      // we get to null, when we get to end of list, set next to 
+      // new node.
+      else {
+        let tempNode = this.head;
+        while (tempNode.next !== null) {
+          tempNode = tempNode.next;
+        }
+        tempNode.next = new single_Node(item, null);
+      }
+    
+    }
+  
+    // to retrieve a node, you start at the beginning and 
+    // move thru the list until that node is found
+    find(item) {
+      // Start at the head
+      let currNode = this.head;
+      // If the list is empty
+      if (!this.head) {
+        return null;
+      }
+      // Check for the item 
+      while (currNode.value !== item) {
+        /* Return null if it's the end of the list 
+                 and the item is not on the list */
+        if (currNode.next === null) {
+          return null;
+        }
+        else {
+          // Otherwise, keep looking 
+          currNode = currNode.next;
+        }
+      }
+      // Found it
+      console.log('Found', item);
+      return currNode;
+    }
+  
+    // in order to delete a node, manipulate the pointers
+    // and node to be removed is automatically garbage collected
+    remove(item) {
+      // If the list is empty
+      if (!this.head) {
+        return null;
+      }
+      // If the node to be removed is head, make the next node head
+      if (this.head.value === item) {
+        // reassign the ponter value to the next node in the list
+        this.head = this.head.next;
+        return;
+      }
+      // Start at the head
+      let currNode = this.head;
+      // Keep track of previous
+      let previousNode = this.head;
+  
+      while ((currNode !== null) && (currNode.value !== item)) {
+        // Save the previous node 
+        previousNode = currNode;
+        currNode = currNode.next;
+      }
+      if (currNode === null) {
+        console.log('Item not found');
+        return;
+      }
+      previousNode.next = currNode.next;
+    }
+  
+    insertBefore(item, newItem) {
+  
+      // Start at the head
+      let currNode = this.head;
+      // Keep track of previous
+      let previousNode = this.head;
+  
+      // If we arent at the end and we havent found our value, go to the next node
+      while ((currNode !== null) && (currNode.value !== item)) {
+        // Save the previous node 
+        previousNode = currNode;
+  
+        // point the the current node at the next node
+        currNode = currNode.next;
+      }
+  
+      //if we have found the item we are lookign for, create new node at current node position
+      if(currNode.value === item){
+        let newNode = new single_Node(newItem, currNode);
+  
+        //point newNode at "current" node position
+        newNode.next = currNode;
+  
+        // point previous node at new Node
+        previousNode.next = newNode;
+      }
+      if (currNode === null) {
+        console.log('Called insertBefore(), but item not found');
+        return;
+      }
+  
+    }
+    insertAfter(item, newItem) {
+      let currNode = this.head;
+      let previousNode = this.head;
+  
+      while (previousNode !== null && previousNode.value !== item) {
+        previousNode = currNode;
+        currNode = currNode.next;
+      }
+  
+      if (previousNode.value === item) {
+        let newNode = new single_Node(newItem, currNode);
+        previousNode.next = newNode;
+      }
+  
+      if (currNode === null) {
+        console.log('Called insertArter(), but item not found');
+        return;
+      }
+    }
+  
+    insertAt(position, item) {
+      let currNode = this.head;
+      let previousNode = this.head;
+      let index = 0;
+  
+      while (currNode !== null && index !== position) {
+        previousNode = currNode;
+        currNode = currNode.next;
+        index++;
+      }
+  
+      if (index === position) {
+        let newNode = new single_Node(item, currNode);
+        previousNode.next = newNode;
+      }
+  
+      if (currNode === null) {
+        console.log('Called insertAt(), but item not found');
+        return;
+      }
+  
+    }
+
+  }
+  
+  module.exports = {SinglyLinkedList, LinkedList};
--- drills.js
+++ drills.js
@@ -1,51 +1,148 @@
-const LinkedList = require('./LinkedList');
-
+const { LinkedList, SinglyLinkedList }  = require('./LinkedList');
 const SSL = new LinkedList();
+const SLL = new SinglyLinkedList();
 
 // .2) Create a singly linked list
 
 main = () => {
-  SSL.insertLast('Apollo');
-  SSL.insertLast('Boomer');
-  SSL.insertLast('Helo');
-  SSL.insertLast('Husker');
-  SSL.insertLast('Starbuck');
-  SSL.insertLast('Tauhida');
-  // SSL.remove("squirrel");
-  SSL.insertBefore('Helo','Y');
-  SSL.insertAfter('Starbuck', 'AfterThing');
-  SSL.insertAt(3, 'Cat');
-  SSL.remove('Tauhida');
+    SSL.insertLast('Apollo');
+    SSL.insertLast('Boomer');
+    SSL.insertLast('Helo');
+    SSL.insertLast('Husker');
+    SSL.insertLast('Starbuck');
+    SSL.insertLast('Tauhida');
+    // SSL.remove("squirrel");
+    SSL.insertBefore('Helo', 'Y');
+    SSL.insertAfter('Starbuck', 'AfterThing');
+    SSL.insertAt(3, 'Cat');
+    SSL.remove('Tauhida');
 };
 
-function display(){
-  console.log(JSON.stringify(SSL));
+single_main = () => {
+    SLL.insertLast('Apollo');
+    SLL.insertLast('Boomer');
+    SLL.insertLast('Helo');
+    SLL.insertLast('Husker');
+    SLL.insertLast('Starbuck');
+    SLL.insertLast('Tauhida');
+    // SSL.remove("squirrel");
+    SLL.insertBefore('Helo', 'Y');
+    SLL.insertAfter('Starbuck', 'AfterThing');
+    SLL.insertAt(3, 'Cat');
+    SLL.remove('Tauhida');
+};
+
+
+//.3) Supplemental functions for a linked list
+
+function display() {
+    //   console.log(JSON.stringify(SSL));
+    let current = SSL.head;
+    let index = 1;
+    while (current !== null) {
+        console.log(current.value);
+        current = current.next;
+        index++;
+    }
 }
 
 function size(SSL) {
-  let current = SSL.head;
-  let index = 1;
+    let current = SSL.head;
+    let index = 1;
 
-  while (current !== null) {
-    current = current.next;
-    index++;
-  }
+    while (current !== null) {
+        current = current.next;
+        index++;
+    }
 
-  console.log(index);
-  return index;
+    console.log(index);
+    return index;
 }
 
 function isEmpty(SSL) {
-  if (SSL.head === null) {
-    console.log('List is empty');
-    return true;
-  }
+    if (SSL.head === null) {
+        console.log('List is empty');
+        return true;
+    }
 
-  console.log('List is not empty');  
-  return false;
+    console.log('List is not empty');
+    return false;
 }
 
-main();
-size(SSL);
-display();
-isEmpty(SSL);
+function findPrevious(SSL, item) {
+    let current = SSL.head;
+    let previous = SSL.head;
+    while (current !== null && current.value !== item) {
+        previous = current;
+        current = current.next;
+    }
+    console.log(previous)
+}
+
+function findLast(SSL) {
+    let current = SSL.head;
+    while (current.next !== null) {
+        previous = current;
+        current = current.next;
+    }
+    console.log(current.value);
+}
+// main();
+// single_main();
+// size(SSL);
+//display();
+// isEmpty(SSL);
+// findPrevious(SSL, "Helo");
+//findLast(SSL);
+
+// .4) Mystery program
+// Analyze the following function (without running it in an IDE) to determine what problem it is trying to solve. What is the time complexity of this algorithm?
+
+function WhatDoesThisProgramDo(lst) {
+    let current = lst.head;
+    while (current !== null) {           
+        let newNode = current;
+        while (newNode.next !== null) {
+            if (newNode.next.value === current.value) {
+                newNode.next = newNode.next.next;
+            }  
+            else {
+                newNode = newNode.next;
+            }
+        }
+        current = current.next;
+    }
+}
+
+//Our explaination is =>
+//The function sets current to the starting position of the list, then checks to see that we are not at the end of the list
+//If not, create new node at current pos, then checks to see if we are at the next to last position of the list,
+//If so, if next value of newNode is set to the value of the node two spots ahead,
+//If not, newNode is set equal to next node's value
+//When reach the second to last position, current is set to the last position
+//Simply Put => the list is forwarding a value from the head to the tail of the list
+//next value to be the value after that
+
+// Complexity = O(n^2) or quadrtaic
+
+//.5) Reverse a list
+
+function reverseList(SLL) {
+
+    let current = SLL.head;
+    let temp1;
+    let previous;
+    console.log(current)
+
+    while(current.next !== null){
+        temp1 = current.next;//next pointer
+        previous = current;//
+        current = temp1;//advances the node position by 1
+        current.next = previous;//change
+    }
+
+    display();
+    
+}
+
+reverseList(SLL);
